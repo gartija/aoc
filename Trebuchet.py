7e669c85cebf5b88ba7b7dc96d79bbb8dbbdf7f3
Francisco Yemail <gartija@gmail.com>
2023-12-02 20:08:37 -0500

    Updated to create part 2 of the Trebuchet puzzle

--- Trebuchet.py
+++ Trebuchet.py
@@ -2,17 +2,20 @@
 with open("trebuchet.txt", "r") as f:
     data = f.readlines()
     sum = 0 
+    map = dict({"0":"0","1":"1","2":"2","3":"3","4":"4","5":"5","6":"6","7":"7","8":"8","9":"9","zero":"0","one":"1","two":"2","three":"3","four":"4","five":"5","six":"6","seven":"7","eight":"8","nine":"9"})
     for line in data:
-        first = None
-        last = None
-        for currentDigit in line:
-            if currentDigit.isnumeric() and first is None:
-                first = currentDigit
-            if currentDigit.isnumeric():
-                last = currentDigit
-        #if last is None:
-        #    last = first
-        sum+= int(first+last)
+        min = [len(line)+1,0]
+        max = [-1,0]
+        for key in map:
+            lf = line.find(key)
+            rf = line.rfind(key)
+            if lf > -1 and lf<min[0]:
+                min[0] = lf
+                min[1] = map[key]
+            if rf > -1 and rf>max[0]:
+                max[0] = rf
+                max[1] = map[key]
+        sum+= int(min[1]+max[1])
     print(sum)
 
 
