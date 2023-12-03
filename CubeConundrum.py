c8f3000913c8bf01d0a169e96fe9efc9e74f95c6
Francisco Yemail <gartija@gmail.com>
2023-12-02 22:42:37 -0500

    Completed day 2, part 2.

--- CubeConundrum.py
+++ CubeConundrum.py
@@ -19,16 +19,36 @@ def possible(input_string,map)->(int,bool):
         print("No match found.")
     return (game,True)
 
+def power(input_string)->int:
+    pattern = r"(?P<color>[0-9]+)[ \t]+\b(?P<amount>blue|green|red)\b"
+    match = re.findall(pattern, input_string)
+    map = dict({})
+    if match:
+        # Print the results
+        for group in match:
+            color = group[1]
+            amount = int(group[0])
+            currentAmount = map.get(color)
+            if currentAmount is None or currentAmount < amount:
+                map.update({color:amount})
+    else:
+        print("No match found.")
+    ret = 1
+    for v in map.values():
+        ret*=v
+    return ret
+
 
 map = dict({"red":12, "green":13,"blue":14})
 with open("cubeconundrum.txt", "r") as f:
     data = f.readlines()
     sum = 0
     for line in data:
+        sum+=power(line)
         #print(line)
-        pos = possible(line,map)
-        if pos[1] is True:
-            sum+=pos[0]
+        #pos = possible(line,map)
+        #if pos[1] is True:
+        #    sum+=pos[0]
     print(sum)
 
 
