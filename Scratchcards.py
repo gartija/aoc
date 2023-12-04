5b215acd6a93951b9789c262ca80db16f5f7631b
Francisco Yemail <gartija@gmail.com>
2023-12-04 15:17:15 -0500

    Adding solution #2 for day 4.

--- Scratchcards.py
+++ Scratchcards.py
@@ -1,19 +1,23 @@
 with open("scratchcards.txt", "r") as f:
     data = f.readlines()
     sumPoints = 0
+    instances = [1]*len(data)
+    currentGame = 0
     for line in data: 
         winners = line[line.find(':')+1:line.find('|')].split(" ")
         mycards = line[line.find('|')+1:].strip().split(" ")
-        currentPointCount = 0
+        numWinners = 1
         for winner in winners:
             if winner!='':
                 try: 
-                    mycards.index(winner)
-                    if currentPointCount == 0:
-                        currentPointCount = 1
-                    else:
-                        currentPointCount*=2
+                    #Check if the current winner is in my cards
+                    mycards.index(winner)                
+                    #If it is I add the ammount of my instances to the next winning card index
+                    instances[currentGame+numWinners]+=instances[currentGame]
+                    numWinners+=1
                 except ValueError:
-                    print("Not found")
-        sumPoints+=currentPointCount
+                    ValueError.args
+        #Add the current game's instances to the points
+        sumPoints+=instances[currentGame]
+        currentGame+=1
     print(sumPoints)
